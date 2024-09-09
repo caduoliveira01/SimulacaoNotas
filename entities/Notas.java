6fbafaade13e83cb1e40ac164fd07ce76c8810a1
Carlos Oliveira <caduolivera17@gmail.com>
2024-09-09 13:53:25 -0300

    Inicializando projeto

---
+++ entities/Notas.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Notas {
+    public double n1,n2,n3;
+
+    public double Media(){
+        return (n1+n2+n3);
+    }
+
+    public String toString(){
+        
+    }
+}
